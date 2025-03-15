8b9fdafca45e2d459bd7e15864c9f19541c73cb3
ALVARO ANDRES DELGADO OVIEDO <hobbie7899@gmail.com>
2025-03-15 10:07:51 -0500

    Task completed of branch feature/AQ-69-mb-listar-granjas-de-un-usuario-owner

--- app/src/main/java/com/example/monitoreoacua/business/models/Farm.java
+++ app/src/main/java/com/example/monitoreoacua/business/models/Farm.java
@@ -19,9 +19,17 @@ public class Farm {
     @SerializedName("longitude")
     private String longitude;
 
+    @SerializedName("createdAt")
+    private String createdAt;
+
+    @SerializedName("updatedAt")
+    private String updatedAt;
+
     public int getId() { return id; }
     public String getName() { return name; }
     public String getAddress() { return address; }
     public String getLatitude() { return latitude; }
     public String getLongitude() { return longitude; }
+    public String getCreatedAt() { return createdAt; }
+    public String getUpdatedAt() { return updatedAt; }
 }
