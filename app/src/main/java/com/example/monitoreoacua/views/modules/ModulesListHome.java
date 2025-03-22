9e2c1aadb8d6738c81f379a7488e3986b29dac15
Angie Montilla <mlangie@unicauca.edu.co>
2025-03-22 11:38:46 -0500

    Add new  interface of list moduless

---
+++ app/src/main/java/com/example/monitoreoacua/views/modules/ModulesListHome.java
@@ -0,0 +1,62 @@
+package com.example.monitoreoacua.views.modules;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageButton;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.monitoreoacua.R;
+import com.example.monitoreoacua.views.farms.ListFarmsActivity;
+import com.example.monitoreoacua.views.menu.ClosesectionActivity;
+import com.example.monitoreoacua.views.menu.SupportActivity;
+
+public class ModulesListHome extends AppCompatActivity {
+
+    // Declaration of navigation bar elements
+    private AppCompatImageButton navHome, navSettings, navProfile, navCloseSesion;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_modules_list_home);
+        
+        Log.d("ModulesListHome", "Activity started");
+        Toast.makeText(this, "Bienvenido a Módulos", Toast.LENGTH_SHORT).show();
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+
+        });
+
+        // Initialize navigation bar elements
+        navHome = findViewById(R.id.navHome);
+        // navSettings = findViewById(R.id.navSettings);
+        navProfile = findViewById(R.id.navProfile);
+        navCloseSesion = findViewById(R.id.navCloseSesion);
+
+        // Events for the navigation bar
+        navHome.setOnClickListener(v -> {
+            Intent intent = new Intent(ModulesListHome.this, ListFarmsActivity.class);
+            startActivity(intent);
+        });
+
+        navProfile.setOnClickListener(v -> {
+            Intent intent = new Intent(ModulesListHome.this, SupportActivity.class);
+            startActivity(intent);
+        });
+
+        navCloseSesion.setOnClickListener(v -> {
+            Intent intent = new Intent(ModulesListHome.this, ClosesectionActivity.class);
+            startActivity(intent);
+        });
+    }
+}
